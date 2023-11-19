80f25447a7edb2ff709c90a6cf2899933b846b11
Daniel <danielpp95@gmail.com>
2023-11-19 16:31:41 +0000

    feat: improve tree components

--- src/components/pageBuilder/TreeComponents.jsx
+++ src/components/pageBuilder/TreeComponents.jsx
@@ -9,67 +9,77 @@ export default function TreeComponents({
     return (
         <div className='page-builder--tree-components'>
             <h2>Components Tree</h2>
-            <RenderLinkedComponentsTree
-                component={linkedComponents.find(x => x.id === 0)}
-                components={[...linkedComponents]}
-                addNestedComponentTo={addNestedComponentTo}
-                removeNestedComponent={removeNestedComponent}
-                selectComponent={selectComponent}/>
+            <ul>
+                <li>
+                <RenderLinkedComponentsTree
+                    component={linkedComponents.find(x => x.id === 0)}
+                    components={[...linkedComponents]}
+                    addNestedComponentTo={addNestedComponentTo}
+                    removeNestedComponent={removeNestedComponent}
+                    selectComponent={selectComponent}/>
+                </li>
+            </ul>
         </div>
     )
 }
-
+// ul > li > details > summary, ul > li -> details -> summary, ul > li >p
 function RenderLinkedComponentsTree({
     component,
     components,
     deepLevel = 0,
-    parentId = null,
     addNestedComponentTo,
     removeNestedComponent,
     selectComponent})
 {
-    deepLevel++
-
-    let [showChildren, setShowChildren] = useState(true);
-
-    let toggleChildren = () => setShowChildren(!showChildren);
-
     const children = components.filter(x => x.parentId === component.id);
 
-    return <div>
-        <RenderComponent
-          component = {component}
-          addNestedComponentTo = {addNestedComponentTo}
-          removeNestedComponent = {removeNestedComponent}
-          deepLevel={deepLevel}
-          selectComponent={selectComponent} />
-        {
-            children.length > 0 && children
-                .map(x => (
-                    <RenderLinkedComponentsTree
-                        key={x.id}
-                        component={x}
-                        components = {components}
-                        deepLevel = {deepLevel}
-                        parentId = {component.id}
-                        addNestedComponentTo={addNestedComponentTo}
-                        removeNestedComponent={removeNestedComponent}
-                        selectComponent={selectComponent}
-                    />))
-        }
-    </div>
+    if (children.length == 0) {
+        return <RenderComponent
+            component = {component}
+            addNestedComponentTo = {addNestedComponentTo}
+            removeNestedComponent = {removeNestedComponent}
+            selectComponent={selectComponent} />
+    }
+
+    return (
+        <details open>
+            <summary>
+                <RenderComponent
+                    component = {component}
+                    addNestedComponentTo = {addNestedComponentTo}
+                    removeNestedComponent = {removeNestedComponent}
+                    selectComponent={selectComponent} />
+            </summary>
+            <ul>
+                {
+                    children.length > 0 && children
+                        .map(x => (
+                            <li>
+                                <RenderLinkedComponentsTree
+                                    key = {x.id}
+                                    component = {x}
+                                    components = {components}
+                                    deepLevel = {deepLevel}
+                                    parentId = {component.id}
+                                    addNestedComponentTo = {addNestedComponentTo}
+                                    removeNestedComponent = {removeNestedComponent}
+                                    selectComponent = {selectComponent}
+                                />
+                            </li>
+                    ))
+                }
+            </ul>
+        </details>
+    )
 }
 
 function RenderComponent({
     component,
     addNestedComponentTo,
     removeNestedComponent,
-    deepLevel,
     selectComponent})
 {
-    let gap = '-'.repeat(deepLevel);
-    
-    return <div key={component.id}>
+    return <div key={component.id} className='tree-item'>
         <p>
             {component.name}
         {
